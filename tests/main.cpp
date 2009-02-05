cde26d0f4d45b3b333316fcbc5696946c8e70b5c
Antti Humalamäki <viagra.salesman@gmail.com>
2009-02-05 22:42:16 +0000

    tetriksen vanha config luokka uudelleenkäyttöä hehe :D

--- tests/main.cpp
+++ tests/main.cpp
@@ -1,19 +1,9 @@
-int WINAPI WinMain(HINSTANCE hinstance,
-HINSTANCE prevInstance,
-PSTR cmdLine,
-int showCmd)
-{
-if(!d3d::InitD3D(hinstance,
-800, 600, true, D3DDEVTYPE_HAL, &Device))
-{
-::MessageBox(0, "InitD3D() - FAILED", 0, 0);
-return 0;
+#include "./Config.h"
+
+int main(void) {
+  SConfig * settings = &SConfig::getInstance();
+  settings->setFilename("testi.ini");
+  settings->readFile();
+  settings->printSettings();
+  return 0;
 }
-if(!Setup())
-{
-::MessageBox(0, "Setup() - FAILED", 0, 0);
-return 0;
-}
-d3d::EnterMsgLoop( Display );
-Cleanup();
-Device->Release();
