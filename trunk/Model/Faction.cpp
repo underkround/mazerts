cd170a170fed78a45f2f6df4e31217351069c896
Antti Humalamäki <viagra.salesman@gmail.com>
2009-02-14 13:17:49 +0000

    Faction base

---
+++ trunk/Model/Faction.cpp
@@ -0,0 +1,11 @@
+#include "Faction.h"
+
+Faction::Faction(void)
+{
+	m_Resource = 0;
+	m_Energy = 0;
+}
+
+Faction::~Faction(void)
+{
+}
---
+++ trunk/Model/Faction.h
@@ -0,0 +1,19 @@
+#pragma once
+/**
+ * Faction encapsulates player or AI in game, keeping track of its
+ * resources and stuff.
+ *
+ * $Revision$
+ * $Date$
+ * $Id$
+ */
+class Faction
+{
+public:
+	Faction(void);
+	virtual ~Faction(void);
+
+private:
+	int m_Resource;
+	int m_Energy;
+};
